c6b366f88390de70dca28a501b212d0fb39463a4
hbw <sp9512@naver.com>
2022-04-27 17:07:37 +0900

    add bj12015 - Greedy+Binary Search - H

---
+++ bj12015.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+int n;
+int arr[1000001];
+int cache[1000001];
+bool find_gap(int gap);
+int main(){
+    cin >> n;
+    memset(arr,-1,sizeof(int)*1000001);
+    memset(cache,-1,sizeof(int)*1000001);
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    int index=0;
+    for(int i=0;i<n; i++)
+    {
+        if( cache[index] == -1 || cache[index] < arr[i])
+            cache[++index] = arr[i];
+        else
+        {
+            cache[lower_bound(cache,cache+index,arr[i])-cache] = arr[i];
+        }
+    }
+    cout << index;
+    return 0;
+}
+
