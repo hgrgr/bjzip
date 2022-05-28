6a8eef84605a92681e38d06d1f2ed14a8f2bbb1d
hbw <sp9512@naver.com>
2022-05-28 16:56:26 +0900

    add bj16235 - BFS,DFS - G - G5

---
+++ bj16235_BFS.cpp
@@ -0,0 +1,96 @@
+//BFS
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+using namespace std;
+int n,L,R;
+int pan[51][51];
+vector<vector<int> > adj;
+bool visit[2501];
+int total;
+int total_sum;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n >> L >> R;
+    memset(pan,-1,sizeof(pan));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin >> pan[i][j];
+        }
+    }
+    int day = 0;
+    while(1){
+        //open line_c
+        adj.clear();
+        adj.resize(n*n+1);
+        memset(visit,false,sizeof(visit));
+        int line_ck = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                //ck right 
+                if(j+1 < n){
+                    if(abs(pan[i][j] - pan[i][j+1]) >= L && abs(pan[i][j] - pan[i][j+1]) <= R){
+                        adj[i*n+j].push_back(i*n+(j+1));
+                        adj[i*n+(j+1)].push_back(i*n+j);
+                        line_ck = 1;
+                    }
+                }
+                //ck down
+                if(i+1 < n){
+                    if(abs(pan[i][j] - pan[i+1][j]) >= L && abs(pan[i][j] - pan[i+1][j]) <= R){
+                        adj[i*n+j].push_back((i+1)*n+j);
+                        adj[(i+1)*n+j].push_back(i*n+j);
+                        line_ck = 1;
+                    }
+                }
+            }
+        }
+        if(line_ck == 0){
+            cout << day;
+            return 0;
+        }
+        //  open
+        //  bfs
+        queue<int> q;
+        vector<int> use_v;
+        for(int i=0;i<n*n;i++){
+            total = 0;
+            total_sum = 0;
+            use_v.clear();
+            if(!visit[i])
+                q.push(i);
+            while(!q.empty()){
+                int here = q.front();
+                q.pop();
+                for(int there = 0;there<adj[here].size();there++){
+                    if(!visit[adj[here][there]]){
+                        visit[adj[here][there]] = true;
+                        use_v.push_back(adj[here][there]);
+                        q.push(adj[here][there]);
+                        total++;
+                        int tup = adj[here][there] / n;
+                        int low = adj[here][there] % n;
+                        total_sum += pan[tup][low];
+                    }
+                }
+            }
+            if(total == 0)
+            {
+                continue;
+            }
+            total_sum /= total;
+            for(int j=0;j<use_v.size();j++){
+                int tup = use_v[j] / n; 
+                int low = use_v[j] % n;
+                pan[tup][low] = total_sum;
+            }
+
+        }
+        day++;
+        //  cal num
+    }
+    return 0;
+}
