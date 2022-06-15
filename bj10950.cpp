b3e5da74288908941084d1833a05b01ab83eb242
hbw <sp9512@naver.com>
2022-06-15 21:55:39 +0900

    add bj10950

---
+++ bj10950.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int tcase;
+	cin >> tcase;
+	for (int i = 0; i < tcase; i++) {
+		int a, b;
+		cin >> a >> b;
+		cout << a + b << endl;
+	}
+	return 0;
+}
