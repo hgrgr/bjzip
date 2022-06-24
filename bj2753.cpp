c0dfb76992d412b360c163fbfc7fcbaf761da533
hbw <sp9512@naver.com>
2022-06-24 22:32:26 +0900

    add bj2753 - G - B

---
+++ bj2753.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+	int num;
+	cin >> num;
+	if((num % 4 == 0) && (num % 100 != 0 | num % 400 == 0))
+		cout << 1;
+	else
+		cout << 0;
+	return 0;
+}
